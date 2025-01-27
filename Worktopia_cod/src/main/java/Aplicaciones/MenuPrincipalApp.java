dcbe30d1bc626fc59bd040d8382ad3b75e2032ce
denox74 <denox_dnx@hotmail.com>
2025-01-27 19:56:43 +0000

    dale

---
+++ Worktopia_cod/src/main/java/Aplicaciones/MenuPrincipalApp.java
@@ -0,0 +1,16 @@
+package Aplicaciones;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MenuPrincipalApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
